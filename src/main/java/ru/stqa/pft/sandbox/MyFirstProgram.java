50006f97dc2e65a95f2da8c4061bd6436dd0727f
KamMV <M.Kamyshev@bssys.com>
2021-03-24 14:32:49 +0400

    Initial commit

---
+++ src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+
+    public static void main (String[] args) {
+        hello("world");
+
+        Square s =new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("Площадь прямоугол со сторонами  " + r.a + " и " + r.b + " = " + r.area());
+
+    }
+
+    public static void hello(String somebody){
+        System.out.println("hello " + somebody);
+    }
+
+
+}
